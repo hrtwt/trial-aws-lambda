ac1e68190e0c26950db9663371ca8f47f4f55b73
hrtwt <29752757+hrtwt@users.noreply.github.com>
2024-06-13 10:38:38 +0900

    install tsc

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+
+export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+  let response: APIGatewayProxyResult;
+  try {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({
+        message: 'hello world',
+      }),
+    };
+  } catch (err: unknown) {
+    console.error(err);
+    response = {
+      statusCode: 500,
+      body: JSON.stringify({
+        message: err instanceof Error ? err.message : 'some error happened',
+      }),
+    };
+  }
+  return response;
+};
