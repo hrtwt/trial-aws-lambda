d10b049e0a4fd6d222730fe2d142c78c35bff144
hrtwt <29752757+hrtwt@users.noreply.github.com>
2024-06-13 11:06:10 +0900

    impl

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,47 @@
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda';
+import { createHash } from 'crypto'
+
+
+const fetchAsBuffer = async (url: string) => {
+  const res = await fetch(url);
+  const arrayBuffer = await res.arrayBuffer();
+  return Buffer.from(arrayBuffer);
+}
+
+const sha256hash = (buffer: Buffer) => {
+  return createHash('sha256').update(buffer).digest('hex')
+}
 
 export const lambdaHandler = async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
-  let response: APIGatewayProxyResult;
   try {
-    response = {
+    if (!event?.body) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: 'Bad Request',
+        }),
+      }
+    }
+    const body = JSON.parse(event.body);
+
+    const blob = await fetchAsBuffer(body.url);
+    const hash = sha256hash(blob);
+
+    return {
       statusCode: 200,
       body: JSON.stringify({
-        message: 'hello world',
+        url: body.url,
+        hash,
+        size: Buffer.byteLength(blob)
       }),
     };
   } catch (err: unknown) {
     console.error(err);
-    response = {
+    return {
       statusCode: 500,
       body: JSON.stringify({
         message: err instanceof Error ? err.message : 'some error happened',
       }),
     };
   }
-  return response;
 };
